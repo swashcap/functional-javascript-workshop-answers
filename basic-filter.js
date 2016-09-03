87747b511a531c1ff9119f9c8d465d74a1d4e33d
Cory Reed <swashcap@gmail.com>
2016-09-03 14:39:37 -0700

    Add basic filter answer.

---
+++ basic-filter.js
@@ -0,0 +1,10 @@
+'use strict';
+
+function getShortMessages(messages) {
+  return messages
+    .filter(m => m.message.length < 50)
+    .map(m => m.message);
+}
+
+module.exports = getShortMessages;
+
